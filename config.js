9515b901fa218cd590d9e21c8f83724be5f67e33
erosiano <edwardrossiano@gmail.com>
2022-11-19 18:01:21 -0500

    Correccion de variables de servidor del backend

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 import dotenv from "dotenv";
 //require('dotenv').config()
-dotenv.config({});
-export const port = process.env.port || 3002;
-export const url =  process.env.uri || "localhost:3001";
+
+
+
+//dotenv.config({});
+//port = "3001";
+const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.1ifv0dg.mongodb.net/thehackermark";
+
+//export const port = process.env.port || 3002;
+//export const url =  process.env.uri || "localhost:3001";
+export const port = 3001 || 3002;
+export const url =  uri || "localhost:3001";
