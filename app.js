d5cd93a28989ddf63c4debc3a5a582cfb6e826a7
erosiano <edwardrossiano@gmail.com>
2022-11-20 16:42:39 -0500

    Revision de origenes

--- app.js
+++ app.js
@@ -5,29 +5,18 @@ import cors from "cors";
 import userRoutes from "./server/routes/users.routes.js";
 import productRoutes from "./server/routes/products.routes.js";
 
-const app = express();
-app.use(express.json());
-app.use(userRoutes);
-app.use(productRoutes);
 
-//CORS
-/* var corsOptions = {
-      origin: "https://erossiano.github.io/"
-  }; */
-//app.use(cors());
- // app.use(cors({ origin: true , credentials :  false}));
-/*  const whitelist = ["https://erossiano.github.io", "http://localhost:3000", "https://erossiano.github.io/thehackermark/"]; */
-/*  const corsOptions = {
-   origin: "*",
-   credentials: false,
- } */
- var corsOptions = {
-  AllowedOrigins: 'https://erossiano.github.io/thehackermark',
+var corsOptions = {
+  origin: 'https://erossiano.github.io/thehackermark',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
- app.use(cors(corsOptions));
+const app = express();
 
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(userRoutes);
+app.use(productRoutes);
 // Add Access Control Allow Origin headers
 /* app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
