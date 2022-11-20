e19ac8784b60ea9de2a4733ff117b5145571c23b
erosiano <edwardrossiano@gmail.com>
2022-11-20 17:49:19 -0500

    Revision de origenes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(productRoutes);
    credentials: false,
  } */
  var corsOptions = {
-  origin: 'https://erossiano.github.io/thehackermark',
+  origin: 'https://erossiano.github.io',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   //methods: "GET, PUT, POST, DELETE"
 }
