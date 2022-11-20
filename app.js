17e2d36839ba6a6469e8fc144a6d25b710bf0054
erosiano <edwardrossiano@gmail.com>
2022-11-20 17:50:44 -0500

    Revision de origenes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(productRoutes);
    credentials: false,
  } */
  var corsOptions = {
-  origin: 'https://erossiano.github.io',
+  origin: '*',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   //methods: "GET, PUT, POST, DELETE"
 }
