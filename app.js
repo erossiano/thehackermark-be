fe8a3db1167b0a9a389360256755173d9e9446b6
erosiano <edwardrossiano@gmail.com>
2022-11-20 14:12:53 -0500

    Revision de origenes

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ var corsOptions = {
 
   app.use(cors({ origin: true , credentials :  false}));
 //app.use(cors(corsOptions));
-  
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 export default app;
